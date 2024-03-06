2b0db15ea603ec3f8c59a16f53b384cd9ccdc722
ehdrjs13 <ehdrjs070102@gmail.com>
2024-03-06 23:16:24 +0900

    add javascript

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-function
+function enlistButton(){
+    alert('지원 기간이 아닙니다. ')
+
+    //신청 사이트 나오면 수정 필요
+    var link = 'https://google.com';
+ 
+    location.href=link;
+    location.replace(link);
+    window.open(link)
+
+}
+
+
+//Button Click Effect
+let Button = document.getElementById('enlist')
+
+
+Button.addEventListener('touchstart', function(){
+    console.log('click!');
+    this.style.backgroundColor = '#28558c';
+});
+Button.addEventListener('touchend', function(){
+    this.style.backgroundColor = '#4193f7';
+});
+
+
